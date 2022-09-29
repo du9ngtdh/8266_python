f239c195d5e3dc4b2d6b6d5447a0e723042daf66
du9ngtdh <103344064+du9ngtdh@users.noreply.github.com>
2022-09-29 22:26:24 +0700

    Merge branch 'master' of https://github.com/du9ngtdh/8266_python

--- blink.py
+++ blink.py
@@ -1,29 +1,27 @@
 import time
-
-
 class Blink:
     _begin = 0
-    _val = 0
+    _value = 0
     _hight = 0
     _low = 0
     _tong = 0
     def __init__(e, hz: float = 1, duty: float = 50) -> None:
-        e._tong = 1000/hz
-        e._hight = (duty/100)*e._tong
-        e._low = e._tong - e._hight
+        e.setting(hz,duty)
 
     def setting(e, hz: float = -1, duty: float = -1) -> None:
         if hz != -1:
             e._tong = 1000/hz
         if duty != -1:
             e._hight = (duty/100)*tong
-            e._low = tong = e._hight
+        else:
+            e._hight = (50/100)*e._tong
+        e._low = tong = e._hight
 
     def value(e, togle: int = 1) -> int:
         if togle == 1:
-            return e._val
+            return e._value
         else:
-            if e._val == 0:
+            if e._value == 0:
                 return 1
             else:
                 return 0
@@ -31,15 +29,15 @@ class Blink:
     def available(e) -> bool:
         now = time.ticks_ms()
         error = now - e._begin
-        if (e._val == 0):
+        if (e._value == 0):
             islow = error > e._low
             if islow:
                 e._begin = now
-                e._val = 1
+                e._value = 1
             return islow
         else:
             ishight = error > e._hight
             if ishight:
                 e._begin = now
-                e._val = 0
+                e._value = 0
             return ishight
